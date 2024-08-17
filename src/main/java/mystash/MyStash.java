c320dd860926a468e34ce9a23de3b2e89d3b5e05
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-08-17 13:26:26 +0300

    Update MyStash.java

--- src/main/java/mystash/MyStash.java
+++ src/main/java/mystash/MyStash.java
@@ -3,18 +3,15 @@ package mystash;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyStash extends JavaPlugin {
-    private DataManager dataManager;
 
     @Override
     public void onEnable() {
-        dataManager = new DataManager(this);
-        getCommand("mystash").setExecutor(new MyStashCommand(this, dataManager));
-        getServer().getPluginManager().registerEvents(new InventoryCloseListener(dataManager), this);
-        dataManager.loadData();
+        getServer().getPluginManager().registerEvents(new InventoryCloseListener(), this);
+        getLogger().info("MyStash has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        dataManager.saveData();
+        getLogger().info("MyStash has been disabled!");
     }
 }
